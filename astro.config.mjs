288bafc7fabc9c5b0527bcaccd9b11ae17959f6c
Thiago Silva <contato@thluiz.com>
2026-04-17 22:25:51 +0100

    Add S3+CloudFront deploy for vingtsun.thluiz.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://vingtsun.thluiz.com',
 	integrations: [
 		starlight({
 			title: 'Ving Tsun: A System Guide',
